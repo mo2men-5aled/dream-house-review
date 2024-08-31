39fbb3e490b6e366e9634da25218fdf5c3e4c7f9
Momen Khaled <momenk208@gmail.com>
2024-08-30 20:30:06 -0700

    created the modal template and its header

--- src/app/components/house-parent/house-parent.component.ts
+++ src/app/components/house-parent/house-parent.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
 
+import { PreStepComponent } from '../pre-step/pre-step.component';
+import { ProbComponent } from '../prob/prob.component';
+import { SelectionComponent } from '../selection/selection.component';
+import { SubmissionComponent } from '../submission/submission.component';
+import { NgIf, NgStyle } from '@angular/common';
+
 @Component({
   selector: 'app-house-parent',
-  standalone: true,
-  imports: [],
   templateUrl: './house-parent.component.html',
-  styleUrl: './house-parent.component.css'
+  standalone: true,
+  imports: [
+    PreStepComponent,
+    ProbComponent,
+    SelectionComponent,
+    SubmissionComponent,
+    NgIf,
+    NgStyle,
+  ],
+  styleUrls: ['./house-parent.component.css'],
 })
 export class HouseParentComponent {
+  currentStep: number = 1;
+  totalSteps: number = 4; // Adjust based on the total number of steps
 
+  get progressWidth(): string {
+    return `${(this.currentStep / this.totalSteps) * 100}%`;
+  }
+
+  handleProceed = () => {
+    this.nextStep();
+  };
+
+  nextStep() {
+    if (this.currentStep < 4) {
+      this.currentStep++;
+    }
+  }
+
+  previousStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
+
+  finish() {
+    alert('Review Submitted!');
+  }
 }
