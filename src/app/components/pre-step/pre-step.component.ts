112430fd789186a4dccbc1047308360514a03ab8
Momen Khaled <momenk208@gmail.com>
2024-08-30 20:31:15 -0700

    created the first step of the modal (pre-step) to select the announcement type

--- src/app/components/pre-step/pre-step.component.ts
+++ src/app/components/pre-step/pre-step.component.ts
@@ -1,12 +1,23 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-pre-step',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgIf],
   templateUrl: './pre-step.component.html',
-  styleUrl: './pre-step.component.css'
+  styleUrl: './pre-step.component.css',
 })
 export class PreStepComponent {
+  selectedType: string | null = null;
+  @Input() handleProceed: () => void;
 
+  constructor() {
+    this.handleProceed = () => {};
+  }
+
+  handleTypeSelection(type: string) {
+    this.selectedType = type;
+  }
 }
