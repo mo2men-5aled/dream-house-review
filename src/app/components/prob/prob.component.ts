7e88b63050754b1c1c0ed24bfcf13dc953755977
Momen Khaled <momenk208@gmail.com>
2024-08-30 20:33:09 -0700

    created second step of the modal it internally conssits of two other steps (1st is to select the place, 2nd is to select the appartment type)

--- src/app/components/prob/prob.component.ts
+++ src/app/components/prob/prob.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-prob',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './prob.component.html',
-  styleUrl: './prob.component.css'
+  styleUrl: './prob.component.css',
 })
 export class ProbComponent {
+  @Input() handleProceed: () => void;
+  currentStage: number = 1;
 
+  constructor() {
+    this.handleProceed = () => {};
+  }
+
+  // this step has internally 2 steps
+  nextStageOfStepThree() {
+    if (this.currentStage < 3) {
+      this.currentStage++;
+    }
+  }
 }
