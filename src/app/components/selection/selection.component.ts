0c0fb4a55930d7be4301b196e4f8dbddec9ffca7
Momen Khaled <momenk208@gmail.com>
2024-08-30 20:34:27 -0700

    created the third step of the modal it also consists of two steps (1st is to describe, 2nd is to add the reviews)

--- src/app/components/selection/selection.component.ts
+++ src/app/components/selection/selection.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-selection',
   standalone: true,
   imports: [],
   templateUrl: './selection.component.html',
-  styleUrl: './selection.component.css'
+  styleUrl: './selection.component.css',
 })
 export class SelectionComponent {
+  selectedPlaceType: string | null = null;
+  selectedAppartmentType: string | null = null;
+  currentStage: number = 1;
 
+  @Input() handleProceed: () => void;
+
+  constructor() {
+    this.handleProceed = () => {};
+  }
+
+  handlePlaceTypeSelection(type: string) {
+    this.selectedPlaceType = type;
+  }
+
+  handleAppartmentTypeSelection(type: string) {
+    this.selectedAppartmentType = type;
+  }
+
+  // this step has internally 2 steps
+  nextStageOfStepTwo() {
+    if (this.currentStage < 3) {
+      this.currentStage++;
+    }
+  }
 }
